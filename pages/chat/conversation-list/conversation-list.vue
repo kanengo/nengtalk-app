108a0c0d90363620207f5131b707eb2f9e0a1bfd
从零开始的程序员生活 <793932470@qq.com>
2024-01-23 18:48:50 +0800

    update chat page

---
+++ pages/chat/conversation-list/conversation-list.vue
@@ -0,0 +1,120 @@
+<template>
+	<view>
+		<uni-list >
+			<uni-list-chat v-for="(item) in chatList" 
+				:avatar="item.avator"
+				avatar-circle=true
+				:title="item.nickname"
+				:note="item.message"
+				:badge-text="50"
+				time="2024-01-12 15:23"
+				clickable=true
+			>
+			</uni-list-chat>
+			<!-- <uni-list-item title="列表文字0" note="列表描述信息"></uni-list-item>
+			<uni-list-item v-for="() in chatList" title="列表文字1" note="列表描述信息"></uni-list-item> -->
+		</uni-list>
+	</view>
+</template>
+
+<script setup>
+	import { reactive } from 'vue';
+
+	const chatList = reactive([
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+		{
+			avator: "http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg",
+			nickname: "kanon lee",
+			message: "今天是个好日子~！!",
+			messageTime: 1705993491,
+		},
+	])
+	
+</script>
+
+<style lang="scss">
+@import 'conversation-list.scss'
+</style>
---
+++ pages/settings-bar/settings-bar.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="uni-flex uni-column settings-bar">
+		<view class="settings-bar__haed">
+			<view class="settings-bar__haed__user-info">
+				<image class="settings-bar__user-avator"
+					mode="aspectFill"
+					src="http://chikii-game-client-sg.oss-ap-southeast-1.aliyuncs.com/upload/beta/icon/5FDF/4E20/5FDFDAAE91B774E20.jpg"
+				></image>
+				<view class="settings-bar__user-text">
+					<text>kanonlee</text>
+				</view>
+				<view class="settings-bar__user-text">
+					<text>12328941283901289 </text>
+				</view>
+			</view>
+		</view>
+		
+		<view class="settings-bar--border"></view>
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss">
+@import 'settings-bar.scss'
+</style>
